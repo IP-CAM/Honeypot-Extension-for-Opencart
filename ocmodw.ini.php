4496a4e76cf4a6ed6ba50a5b348bb31ca2e031c4
akaunderr <akaunderr@gmail.com>
2024-02-27 16:13:04 +0200

    updated aux. files

---
+++ ocmodw.ini.php
@@ -0,0 +1,25 @@
+<?php
+
+define('MODVERS', '1.5.1');
+define('MODYEAR', '2023');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=45552');
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-honeypot');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICLINK', 'https://raw.githubusercontent.com/ocmod-space/ocmod-honeypot/main/EULA.txt');
+
+///
+
+define('FCLDIR', '_fcl');
+define('FCLIGNORE', '.fclignore'); // ignorelist for fcl
+define('SRCDIR', 'src');
+define('ZIPDIR', 'zip');
+define('ZIPEXT', '.ocmod.zip');
+
+define('MDIR', 'module'); // dir with module
+define('ADIR', 'addons'); // dir with addons
